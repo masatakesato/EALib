9a4ecfed659481d342b946a76157b41fb1a19578
masatake1115 <masatake1115@hotmail.co.jp>
2021-08-22 12:38:06 +0900

    Pending: Constraint implementation. TODO: Init Population using DesignParamArray

--- test/testConstraint/testConstraint_main.cpp
+++ test/testConstraint/testConstraint_main.cpp
@@ -1,6 +1,7 @@
 ﻿#include	<oreore/common/Utility.h>
 #include	<oreore/common/TString.h>
 #include	<oreore/mathlib/MathLib.h>
+#include	<oreore/container/Array.h>
 
 
 #include	<ealib/IChromosome.h>
@@ -8,28 +9,34 @@ using namespace ealib;
 
 
 // http://www.dais.is.tohoku.ac.jp/~shioura/teaching/mp07/mp07-12.pdf
+// http://www.orsj.or.jp/~archive/pdf/bul/Vol.40_02_112.pdf
 
 
-template < typename T >
-class IConstraint
+// 一旦中止. 以下3点の問題解決が必要. 簡易的な計算式のノードグラフ必要. 2021.08.22
+
+// 可変長パラメータの制約関数をどう定義するか
+
+// 入出力変数群をどう定義するか
+
+// 入出力変数群と制約関数との接続をどう実現するか
+
+
+class IConstraintOperator
 {
 public:
 
-	T Execute( const IChromosome* chromosome ) = 0;
-
-};
-
-
-
-
-template < typename T >
-class PenaltyConstraint : IConstraint<T>
-{
-public:
-
-	virtual T Execute( const IChromosome* chromosome )
+	template < typename ... Args >
+	void G( const IChromosome* chromosome, Args... args )
 	{
-		return 0;
+		G( chromosome );
+		G( args... );
+	}
+
+
+
+	virtual void G( const IChromosome* chromosome )
+	{
+		tcout << "IConstraintOperator::G( const IChromosome* chromosome );\n";
 	}
 
 };
@@ -37,57 +44,33 @@ public:
 
 
 
-
-
-
-
-template < typename T >
-T G( T& param )
+class MyConstraintOp : public IConstraintOperator
 {
+public:
 
-	return 0;
-}
+	using IConstraintOperator::G;
+
+	virtual void G( IChromosome* chromosome, int threshold )
+	{
+		tcout << "MyConstraintOp::G( IChromosome* chromosome, int threshold )..." << threshold << tendl;
+
+	}
+};
 
 
 
-// penalty function for "<= 0"
-template < typename T >
-T PenaltyFunction_LessThan( float mu, int numparams, const T x[] )
+class MyConstraintOp2 : public IConstraintOperator
 {
-	T P = 0;
+public:
 
-	for( int i=0; i<numparams; ++i )
-		P += Max( G(x[i]), 0 );
-	
-	return P;
-}
+	using IConstraintOperator::G;
 
+	virtual void G( const tstring& tstr )
+	{
+		tcout << "MyConstraintOp2::G( int threshold )..." << tstr << tendl;
 
-
-// penalty function for "== 0"
-template < typename T >
-T PenaltyFunction_Equal( float mu, int numparams, const T x[] )
-{
-	T P = 0;
-
-	for( int i=0; i<numparams; ++i )
-		//P += abs( G(x[i]) );
-		P += pow( G(x[i]), 2 );
-	
-	return P;
-}
-
-
-
-
-template < typename T >
-T BarrierFunction( float mu, int numparams, const T x[] )
-{
-
-
-
-	return 0;
-}
+	}
+};
 
 
 
@@ -96,8 +79,138 @@ T BarrierFunction( float mu, int numparams, const T x[] )
 int main()
 {
 
+	MyConstraintOp op;
+	op.G( nullptr, 6 );
 
+
+	MyConstraintOp2 op2;
+	op2.G( _T("gfdsgfds") );
+
+
+	IConstraintOperator* pOp = &op;
+
+	pOp->G( nullptr, _T("AAAAA") );
 
 
 	return 0;
 }
+
+
+
+
+
+
+
+//template < typename T >
+//T G( const IChromosome* chromosome )
+//{
+//
+//	return (T)0;
+//}
+
+
+//class IConstraint
+//{
+//public:
+//
+//	virtual double Execute( const IChromosome* chromosome ) = 0;
+//
+//
+//	void AddLessThanOperator(){}
+//	void AddEqualOperator(){}
+//
+//};
+
+
+
+
+//// penalty function method
+//class ExteriorPenalty : IConstraint
+//{
+//public:
+//
+//	virtual double Execute( const IChromosome* chromosome )
+//	{
+//		return 0;
+//	}
+//
+//
+//
+//private:
+//
+//	OreOreLib::Array< double* >	m_Inequalities;
+//	OreOreLib::Array< double* >	m_Equalities;
+//
+//
+//
+//	// penalty function for "<= 0"
+//	double AccumulateLessEqual( float mu, int numFuncs, const IChromosome* chromosome )
+//	{
+//		double P = 0;
+//
+//		for( int i=0; i<numFuncs; ++i )
+//			P += Max( G<double>( chromosome ), 0.0 );
+//	
+//		return P;
+//	}
+//
+//
+//
+//	// penalty function for "== 0"
+//	double AccumulateEqual( float mu, int numFuncs, const IChromosome* chromosome )
+//	{
+//		double P = 0;
+//
+//		for( int i=0; i<numFuncs; ++i )
+//			//P += abs( G( chromosome ) );
+//			P += pow( G<double>( chromosome ), 2.0 );
+//	
+//		return P;
+//	}
+//
+//
+//};
+
+
+
+//// Barrier function method
+//class InteriorPenalty : IConstraint
+//{
+//public:
+//
+//	virtual double Execute( const IChromosome* chromosome )
+//	{
+//		return 0;
+//	}
+//
+//
+//
+//private:
+//
+//
+//	// penalty function for "<= 0"
+//	double AccumulateLessEqual( float mu, int numFuncs, const IChromosome* x )
+//	{
+//		double P = 0;
+//
+//		for( int i=0; i<numFuncs; ++i )
+//			P -= 1.0 / G<double>( x );
+//	
+//		return P;
+//	}
+//
+//
+//
+//	// penalty function for ">= 0"
+//	double AccumulateGreaterEqual( float mu, int numFuncs, const IChromosome* x )
+//	{
+//		double P = 0;
+//
+//		for( int i=0; i<numFuncs; ++i )
+//			P -= log( -G<double>( x ) );
+//	
+//		return P;
+//	}
+//
+//
+//};
